d57131882887be53473e09c460bd78124ca9a90f
Shtuka1993 <YuriyShtokaylo@gmail.com>
2024-02-25 21:16:01 +0100

    Create CourseCard component

---
+++ src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Button from 'src/common/Button/Button';
+
+export default function CourseCard({
+	title,
+	description,
+	authors,
+	duration,
+	created,
+}) {
+	return (
+		<>
+			<div>
+				<h3>{title}</h3>
+				<p>{description}</p>
+			</div>
+			<div>
+				<ul>
+					<li>
+						<b>Authors:</b>
+						{authors}
+					</li>
+					<li>
+						<b>Duration:</b>
+						{duration}
+					</li>
+					<li>
+						<b>Created:</b>
+						{created}
+					</li>
+				</ul>
+				<Button text='Show course' />
+			</div>
+		</>
+	);
+}
