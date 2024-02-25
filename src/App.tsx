373842c773faa4b01322f0f542fc08f66c989b77
Shtuka1993 <YuriyShtokaylo@gmail.com>
2024-02-25 22:17:20 +0100

    Render header component inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header/Header';
+
 export default function App() {
-	return <div>React</div>;
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
