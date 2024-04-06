596298a8e88e286d24b1a3defc2d114a672ffaa0
Shtuka1993 <YuriyShtokaylo@gmail.com>
2024-04-06 23:00:19 +0200

    Draft

--- src/services.tsx
+++ src/services.tsx
@@ -1,35 +1,3 @@
-/*import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { addCourse } from './store';
-import { Course } from 'src/types';*/
-
-/*export const GetCourses: () => Course[] | [] = () => {
-	const dispatch = useDispatch();
-	console.log('dispatch');
-	axios
-		.get('http://localhost:4000/courses/all')
-		.then((response) => {
-			return response.data.result;
-		})
-		.catch((error) => {
-			console.error(error.response);
-			return [];
-		});
-};
-
-export const GetAuthors = () => {
-	const dispatch = useDispatch();
-	axios
-		.get('http://localhost:4000/authors/all')
-		.then((response) => {
-			return response.data.result;
-		})
-		.catch((error) => {
-			console.error(error.response);
-			return [];
-		});
-};*/
-
 const coursesUrl = 'http://localhost:4000/courses/all';
 const authorsUrl = 'http://localhost:4000/authors/all';
 
